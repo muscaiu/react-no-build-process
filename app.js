7117db663053deee31f9edbb4195aa139608012f
Cristian Muscalu <muscaiu@yahoo.com>
2018-08-16 12:04:11 +0300

    initial

---
+++ app.js
@@ -0,0 +1,12 @@
+(function () {
+    "use strict";
+  
+    var PruductCustomizer = React.createElement(
+      'div',
+      { className: "customizer" },
+      "Product customizer will go here"
+    );
+      
+    ReactDOM.render(PruductCustomizer, document.getElementById('react-root'));  
+  })();
+  
