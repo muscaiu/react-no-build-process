35af28684ac85698ac881d00eec5caf84b829469
Cristian Muscalu <muscaiu@yahoo.com>
2018-08-16 12:57:44 +0300

    added size selector

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 (function () {
     "use strict";
 
+    function SizeSelector(props) {
+        return (
+            <div className="field-group">
+                <label htmlFor="size-options">Size:</label>
+                <select name="sizeOptions" id="size-options">
+                    <option>7</option>
+                    <option>7.5</option>
+                    <option>9</option>
+                </select>
+            </div>
+        )
+    }
+
     function ProductImage(props) {
-        return <img src="./assets/red.jpg" alt="product image" />)
+        return <img src="./assets/red.jpg" alt="product image" />
     }
 
     function PruductCustomizer(props) {
@@ -10,6 +23,9 @@
             <div className="customizer">
                 <div className="product-image" >
                     < ProductImage />
+                    <div className="selectors" >
+                        < SizeSelector />
+                    </div>
                 </div>
             </div>
         );
